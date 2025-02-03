bafd3aa198c45c9d0471a627f6f4a7318a73a131
innazinchenko <spangle_red@inbox.ru>
2025-02-03 11:18:37 +0100

    Product service implemented

---
+++ src/main/java/app/service/ProductService.java
@@ -0,0 +1,19 @@
+package app.service;
+
+import app.domain.Product;
+
+import java.util.List;
+
+public interface ProductService {
+
+    Product save(Product product);
+    List<Product> getAllActiveProducts();
+    Product getById(Long id);
+    void update(Product product);
+    void deleteById(Long id);
+    void deleteByTitle(String title);
+    void restoreById(Long id);
+    long getActiveProductsTotalCount();
+    double getActiveProductsTotalCost();
+    double getActiveProductsAveragePrice();
+}
